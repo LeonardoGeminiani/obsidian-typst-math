9c984c7902769533afa0c6e63dc76d7205b8e86c
Leonardo Geminiani <leonardo.geminiani7@gmail.com>
2025-02-27 21:23:13 +0100

    add wasm on github

---
+++ pkg/obsidian_rust_plugin_bg.wasm.d.ts
@@ -0,0 +1,29 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_compiler_free: (a: number, b: number) => void;
+export const compiler_new: (a: number, b: number, c: any) => number;
+export const compiler_compile_image: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => [number, number, number];
+export const compiler_compile_svg: (a: number, b: number, c: number, d: number, e: number) => [number, number, number, number];
+export const compiler_add_font: (a: number, b: number, c: number) => void;
+export const qcms_transform_data_rgb_out_lut_precache: (a: number, b: number, c: number, d: number) => void;
+export const qcms_transform_data_rgba_out_lut_precache: (a: number, b: number, c: number, d: number) => void;
+export const qcms_transform_data_bgra_out_lut_precache: (a: number, b: number, c: number, d: number) => void;
+export const qcms_transform_data_rgb_out_lut: (a: number, b: number, c: number, d: number) => void;
+export const qcms_transform_data_rgba_out_lut: (a: number, b: number, c: number, d: number) => void;
+export const qcms_transform_data_bgra_out_lut: (a: number, b: number, c: number, d: number) => void;
+export const qcms_transform_release: (a: number) => void;
+export const qcms_profile_precache_output_transform: (a: number) => void;
+export const qcms_enable_iccv4: () => void;
+export const qcms_profile_is_bogus: (a: number) => number;
+export const qcms_white_point_sRGB: (a: number) => void;
+export const lut_interp_linear16: (a: number, b: number, c: number) => number;
+export const lut_inverse_interp16: (a: number, b: number, c: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
